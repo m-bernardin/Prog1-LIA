8d11464103f7843b0b4ed588ac5789167ae7e984
maddysiwick <maddy.siwick@icloud.com>
2025-12-02 09:16:52 -0500

    continued setting up the test classes

--- RecipeTest.java
+++ RecipeTest.java
@@ -2,8 +2,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-public class StepsTest {
-    public StepsTest() {
+public class RecipeTest {
+    public RecipeTest() {
     }
     @BeforeEach
     public void setUp() {
@@ -11,4 +11,7 @@ public class StepsTest {
     @AfterEach
     public void tearDown() {   
     }
+    @Test
+    public void testSomeRecipeFunctionality() {
+        
 }
