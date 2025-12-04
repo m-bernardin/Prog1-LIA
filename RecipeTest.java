5a7c6624dc44b6e9644bce1edeecb97e1b9bd2f2
maddysiwick <maddy.siwick@icloud.com>
2025-12-04 14:21:24 -0500

    wrote tests for recipe and started simple recipe

--- RecipeTest.java
+++ RecipeTest.java
@@ -3,15 +3,31 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 public class RecipeTest {
+    Recipe recipe;
     public RecipeTest() {
     }
     @BeforeEach
     public void setUp() {
+        Recipe recipe = new Recipe("Grilled Cheese");
     }
     @AfterEach
     public void tearDown() {   
     }
     @Test
-    public void testSomeRecipeFunctionality() {
+    public void testUpVote() {
+        recipe.upVote();
+        assertEquals(1, recipe.getRating());
+    
+    }
+    @Test
+    public void testDownVote() {
+        recipe.downVote();
+        assertEquals(-1, recipe.getRating());
+    }
+    @Test
+    public void testAddTag() {
+        recipe.addTag(Tags.VEGETARIAN)
+        assertTrue(recipe.getTags().contains(Tags.VEGETARIAN));
+    }
         
 }
--- SimpleRecipeTest.java
+++ SimpleRecipeTest.java
@@ -3,12 +3,46 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 public class SimpleRecipeTest {
+    SimpleRecipe simpleRecipe;
     public SimpleRecipeTest() {
     }
     @BeforeEach
     public void setUp() {
+        simpleRecipe = new SimpleRecipe("Grilled Cheese",1);
+        simpleRecipe.addStep(new Recipe(new HashSet(new Ingredient[]{new Ingredient("Sliced Bread",2,Unit.),})"Butter the bread"));
     }
     @AfterEach
     public void tearDown() {   
     }
+    @Test
+    public void testCalculateTime() {
+    
+    }
+    @Test
+    public void testCalculateIngrediants() {
+    
+    }
+    @Test
+    public void testGetIngrediantsAsString() {
+    
+    }
+    @Test
+    public void testGetTimesAsString() {
+    
+    }
+    
+    @Test
+    public void testFormatRecipeAsString() {
+    
+    }
+    @Test
+    public void testScale() {
+    
+    }
+    @Test
+    public void testAddStep() {
+    
+    }
+    
+
 }
