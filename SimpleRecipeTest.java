bafb4f585b2e6326492ab7a7aa48f1562946dd39
maddysiwick <maddy.siwick@icloud.com>
2025-12-05 04:37:35 -0500

    finished tests for simple recipe

--- SimpleRecipeTest.java
+++ SimpleRecipeTest.java
@@ -9,40 +9,42 @@ public class SimpleRecipeTest {
     @BeforeEach
     public void setUp() {
         simpleRecipe = new SimpleRecipe("Grilled Cheese",1);
-        simpleRecipe.addStep(new Recipe(new HashSet(new Ingredient[]{new Ingredient("Sliced Bread",2,Unit.),})"Butter the bread"));
+        simpleRecipe.addStep(new Step(new HashSet(new Ingredient[]{new Ingredient("Sliced Bread",2,Unit.INDIVIDUAL),new Ingredient("Butter",1,Unit.TABLESPOON)}),"Butter the bread",1));
+        simpleRecipe.addStep(new Step(new HashSet(new Ingredient[]{new Ingredient("Cheese slice",2,Unit.INDIVIDUAL)}),"Make a sandwich with the cheese slices",1));
     }
     @AfterEach
     public void tearDown() {   
     }
     @Test
     public void testCalculateTime() {
+        assertEquals(2,simpleRecipe.calculateTime());
     
     }
     @Test
     public void testCalculateIngrediants() {
-    
-    }
-    @Test
-    public void testGetIngrediantsAsString() {
-    
-    }
-    @Test
-    public void testGetTimesAsString() {
+        HashSet<Ingredient> expectedIngredients = new HashSet<>();
+        expectedIngredients.add(new Ingredient("Sliced Bread",2,Unit.INDIVIDUAL));
+        expectedIngredients.add(new Ingredient("Butter",1,Unit.TABLESPOON));
+        expectedIngredients.add(new Ingredient("Cheese slice",2,Unit.INDIVIDUAL));
+        assertEquals(expectedIngredients, simpleRecipe.calculateIngrediants());
     
     }
     
-    @Test
-    public void testFormatRecipeAsString() {
-    
-    }
     @Test
     public void testScale() {
-    
+        Recipe expectedRecipe = new SimpleRecipe("Grilled Cheese",2);
+        expectedRecipe.addStep(new Step(new HashSet(new Ingredient[]{new Ingredient("Sliced Bread",4,Unit.INDIVIDUAL),new Ingredient("Butter",2,Unit.TABLESPOON)}),"Butter the bread",1));
+        expectedRecipe.addStep(new Step(new HashSet(new Ingredient[]{new Ingredient("Cheese slice",4,Unit.INDIVIDUAL)}),"Make a sandwich with the cheese slices",1));
+        assertEquals(expectedRecipe, simpleRecipe.scale(2));
+
+        //scale will now return a new SimpleRecipe object
     }
     @Test
     public void testAddStep() {
-    
+        simpleRecipe.addStep(new Step(new HashSet(),"Fry sandwich on both sides",7));
+        assertEquals(3,simpleRecipe.getSteps().size());
     }
+    
     
 
 }
