b936992bd40779e2563a3f922ff7ca5891011dc2
m-bernardin <mjrb4mjrb@gmail.com>
2025-12-07 21:36:02 -0500

    Reapply "finished test units first version, changed .gitignore to keep junit lib"
    This reverts commit 115d8ebf67522febcfa5a8a9f29b76e977121561.

--- ComplexRecipe.java
+++ ComplexRecipe.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+
 public class ComplexRecipe extends Recipe{
     
+    ArrayList<SimpleRecipe> Subrecipes = new ArrayList<>();
+    
+    public ComplexRecipe(String name, int servings) {
+        super(name, servings);
+    }
 }
