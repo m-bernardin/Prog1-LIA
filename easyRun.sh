9e2e4c0e9813177d46895eb2fb633cb9a8bd48c3
m-bernardin <mjrb4mjrb@gmail.com>
2025-12-15 23:10:37 -0500

    fixed license info

--- easyRun.sh
+++ easyRun.sh
@@ -3,4 +3,8 @@ echo Compiling Java source code
 javac ComplexRecipe.java Ingredient.java InputManager.java Main.java Recipe.java RecipeBook.java SimpleRecipe.java Step.java Tags.java Units.java
 echo Java source code compiled
 echo Running program
+printf 'aSuperGoodName  Copyright (C) 2025  Mathieu Bernardin, Madeleine Siwick
+    This program comes with ABSOLUTELY NO WARRANTY; for details see "COPYING.md."
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; see "COPYING.md" for details.'
 java Main
